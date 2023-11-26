261a065d4f32314ad99f709728cc04e2786d2ec3
RamiOualiEdu <rami.ouali@esprit.tn>
2023-11-26 18:41:38 +0100

    firstcommit

--- src/main/java/tn/esprit/microservice/RoomService/EurekaServerApplication.java
+++ src/main/java/tn/esprit/microservice/RoomService/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package tn.esprit.microservice.RoomService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/tn/esprit/microservice/RoomService/EurekaServerApplicationTests.java
+++ src/test/java/tn/esprit/microservice/RoomService/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package tn.esprit.microservice.RoomService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
