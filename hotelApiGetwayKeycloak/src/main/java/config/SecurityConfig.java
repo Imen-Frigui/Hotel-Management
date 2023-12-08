ed9ceac7225aa1f5dd2d05d126f3d629d7cc2006
Adoulii <omaradouli79@gmail.com>
2023-12-08 16:46:52 +0100

    keycloak fixes

--- hotelApiGetwayKeycloak/src/main/java/config/SecurityConfig.java
+++ hotelApiGetwayKeycloak/src/main/java/config/SecurityConfig.java
@@ -1,27 +1,25 @@
-package config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
-
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity)  throws Exception {
-        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange
-                        .anyExchange().authenticated()
-                ).oauth2ResourceServer((oauth) -> oauth
-                        .jwt(Customizer.withDefaults()))
-                .build();
-    }
-
-
-}
+//package config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity)  throws Exception {
+//        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**").permitAll()
+//                        .anyExchange().authenticated()
+//                ).oauth2ResourceServer((oauth) -> oauth
+//                        .jwt(Customizer.withDefaults()))
+//                .build();
+//    }
+//
+//
+//}
