c8fdb106452e93c5e4d8e570ed6d57bd93726f85
Imen-Frigui <imen.frigui@esprit.tn>
2023-12-10 22:09:38 +0100

    composer+keyclock

--- src/main/java/com/esprit/microservices/reclamation/ReclamationApplication.java
+++ src/main/java/com/esprit/microservices/reclamation/ReclamationApplication.java
@@ -3,7 +3,7 @@ package com.esprit.microservices.reclamation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ReclamationApplication {
